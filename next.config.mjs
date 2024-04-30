6e5a0007fe86b09806d341276898c9fcbd51afb4
Mohammad Kaif <mohammadkaif051197@gmail.com>
2024-04-30 11:34:53 +0530

    added new page

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+// next.config.mjs
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'http://157.245.98.188' : '',
 };
 
 export default nextConfig;
