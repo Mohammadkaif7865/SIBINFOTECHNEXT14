2a17e712121ec8ed2250d9d6637c1de2812ecbc1
Mohammad Kaif <mohammadkaif051197@gmail.com>
2024-05-08 16:17:57 +0530

    some url changes

--- constants/constants.js
+++ constants/constants.js
@@ -1,6 +1,6 @@
-export const BACKEND_URL = 'https://www.sibinfotech.com/live-node-api/';
-export const API_URL = 'https://www.sibinfotech.com/live-node-api/api/';
+export const BACKEND_URL = 'https:/157.245.98.188/api/';
+export const API_URL = 'https:/157.245.98.188/api/api/';
 export const API_TOKEN = 'FgRCHG4OVv8Z1BcrjExKJcqspvTsUTCe';
 
-// export const BASE_URL = 'https://www.sibinfotech.com/';
-export const BASE_URL = 'https://www.sibinfotech.com/';
+// export const BASE_URL = 'https:/157.245.98.188/';
+export const BASE_URL = 'https:/157.245.98.188/';
