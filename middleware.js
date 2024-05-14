c5110e41cde1a6e86f7f95ec2ba93c915c8395be
mohammad kaif <mohammadkaig051197@gmail.com>
2024-05-14 15:43:21 +0530

    some chagnes

--- middleware.js
+++ middleware.js
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 export function middleware(request) {
     const { host } = request.nextUrl;
 
-    if (host === 'sibinfotech') {
+    if (host === 'sibinfotech.com') {
         const url = request.nextUrl.clone();
         url.host = 'www.sibinfotech.com';
         return NextResponse.redirect(url);
