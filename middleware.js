97ca0e40fe0195b4701a29484cbec008d3e9744f
mohammad kaif <mohammadkaig051197@gmail.com>
2024-05-14 16:01:17 +0530

    some chagnes

--- middleware.js
+++ middleware.js
@@ -1,13 +1,11 @@
 import { NextResponse } from 'next/server';
 
-export function middleware(request) {
-    const { host } = request.nextUrl;
-    console.log("DFSDFSDFDFSFSDFSDF", host);
-    // if (host === 'sibinfotech.com') {
-    //     const url = request.nextUrl.clone();
-    //     url.host = 'www.sibinfotech.com';
-    //     return NextResponse.redirect(url);
-    // }
-
+export function middleware(req) {
+    const { nextUrl } = req;
+    if (nextUrl.hostname === 'sibinfotech.com') {
+        const url = req.nextUrl.clone();
+        url.hostname = 'www.sibinfotech.com';
+        return NextResponse.redirect(url);
+    }
     return NextResponse.next();
 }
