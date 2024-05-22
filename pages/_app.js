24e2a6a153d7dde72cc8bcd7b430fa8907272b21
sib-vishal <vishal.maurya@sibinfo.tech>
2024-05-22 11:51:35 +0530

    commit css isses have been fixed

--- pages/_app.js
+++ pages/_app.js
@@ -92,11 +92,18 @@ function MyApp({ Component, pageProps }) {
             as="video/mp4"
           />
           <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+          />
+
+          {/* <link
             rel="preload"
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
             as="style"
             onload="this.onload=null;this.rel='stylesheet'"
-          />
+          /> */}
           <link
             rel="preload"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
@@ -139,7 +146,7 @@ function MyApp({ Component, pageProps }) {
               rel="stylesheet"
             />
           </noscript>
-{/* dd */}
+          {/* dd */}
           {/* <link rel="apple-touch-icon" href="/logo192.png" /> */}
           {/* <link rel="manifest" href="/manifest.json" /> */}
         </Head>
