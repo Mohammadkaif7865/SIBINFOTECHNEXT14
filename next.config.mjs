8542526b0b966bda95a623e7f1b457ac2abff0c1
Mohammad kaif <75516429+Mohammadkaif7865@users.noreply.github.com>
2024-05-24 11:08:19 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -82,7 +82,7 @@ const nextConfig = {
       },
     ];
   },
-  assetPrefix: 'https://www.sibinfotech.com',
+  assetPrefix: 'https://sibinfotechnext-14-git-homepage-mohammadkaif7865s-projects.vercel.app',
   images: {
     domains: ["www.sibinfotech.com"],
   },
