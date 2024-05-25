b6ee83e5a20f6dbe57678b5e3f003ec868cffa7c
sib-vishal <vishal.maurya@sibinfo.tech>
2024-05-25 13:10:28 +0530

    commit unneccery fles removed

--- pages/_app.js
+++ pages/_app.js
@@ -93,8 +93,9 @@ function MyApp({ Component, pageProps }) {
           />
           <link
             rel="stylesheet"
-            href="/assets/css/bundle.css"
-          
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
           />
 
           {/* <link
