dd75376bf59292bc2e3dda5a2348bdfc3c42a7f3
sib-vishal <vishal.maurya@sibinfo.tech>
2024-05-28 16:41:48 +0530

    commit

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           {/* Google Tag Manager */}
-          <script
+          {/* <script
             dangerouslySetInnerHTML={{
               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -15,21 +15,21 @@ class MyDocument extends Document {
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-WSSFNZR');`,
             }}
-          />
+          /> */}
           {/* End Google Tag Manager */}
 
          
         </Head>
         <body>
           {/* Google Tag Manager (noscript) */}
-          <noscript>
+          {/* <noscript>
             <iframe
               src="https://www.googletagmanager.com/ns.html?id=GTM-WSSFNZR"
               height="0"
               width="0"
               style={{ display: "none", visibility: "hidden" }}
             ></iframe>
-          </noscript>
+          </noscript> */}
 
           {/* End Google Tag Manager (noscript) */}
           <Main />
