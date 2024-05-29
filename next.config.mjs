c97d1dc35825c3305af4f6a1ec4732e79761b972
sib-vishal <vishal.maurya@sibinfo.tech>
2024-05-29 15:09:19 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -82,7 +82,7 @@ const nextConfig = {
       },
     ];
   },
-  // assetPrefix: "https://www.sibinfotech.com",
+  assetPrefix: "https://sibinfotechnext-14-git-on-page-seo2-mohammadkaif7865s-projects.vercel.app",
 };
 
 export default nextConfig;
