c7c0a607e33a06771bc389cb59c930f832d27d64
sib-vishal <vishal.maurya@sibinfo.tech>
2024-05-29 11:39:28 +0530

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -101,10 +101,9 @@ function MyApp({ Component, pageProps }) {
             crossorigin="anonymous"
             onload="this.onload=null;this.rel='stylesheet'"
           /> */}
-         
-          {/* <link
-            rel="preconnect"
-            as="style"
+
+          <link
+            rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
             integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
             crossorigin="anonymous"
@@ -116,7 +115,7 @@ function MyApp({ Component, pageProps }) {
               integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
               crossorigin="anonymous"
             />
-          </noscript> */}
+          </noscript>
 
           {/* <link
             rel="preload"
