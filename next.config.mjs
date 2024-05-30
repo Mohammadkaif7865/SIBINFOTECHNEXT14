c6ff546f8da013850ef72b8ff21996c49bfff7c7
sib-vishal <vishal.maurya@sibinfo.tech>
2024-05-30 12:42:56 +0530

    E-Commerce SEO Services Page has been completed

--- next.config.mjs
+++ next.config.mjs
@@ -82,7 +82,8 @@ const nextConfig = {
       },
     ];
   },
-  assetPrefix: "https://www.sibinfotech.com",
+  assetPrefix:
+    "https://sibinfotechnext-14-git-off-pa-f7f7c1-mohammadkaif7865s-projects.vercel.app",
 };
 
 export default nextConfig;
