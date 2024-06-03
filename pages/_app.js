223c7e7976432ec1c7792d8ac6a8eb0b4ca77d07
sib-vishal <vishal.maurya@sibinfo.tech>
2024-06-03 11:32:25 +0530

    commit icon error fixed

--- pages/_app.js
+++ pages/_app.js
@@ -88,60 +88,39 @@ function MyApp({ Component, pageProps }) {
           <meta name="expires" content="never" />
        
 
-          {/* <link
+          <link
             rel="preload"
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-            as="style"
-            onload="this.onload=null;this.rel='stylesheet'"
-          /> */}
-          {/* <link
+            href="/assets/images/banner/banner-video.mp4"
+            as="video/mp4"
+          />
+
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css"
+            integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"
+          />
+
+          <link
             rel="preload"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
             as="style"
-            onload="this.onload=null;this.rel='stylesheet'"
+            onLoad="this.onload=null;this.rel='stylesheet'"
           />
           <link
             rel="preload"
             href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css"
             as="style"
-            onload="this.onload=null;this.rel='stylesheet'"
+            onLoad="this.onload=null;this.rel='stylesheet'"
           />
           <link
             rel="preload"
             href="https://unpkg.com/aos@2.3.1/dist/aos.css"
             as="style"
-            onload="this.onload=null;this.rel='stylesheet'"
-          /> */}
-
-
-          {/* curr */}
-          {/* <link
-            rel="preload"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-            as="style"
-            // onLoad="this.onload=null;this.rel='stylesheet'"
+            onLoad="this.onload=null;this.rel='stylesheet'"
           />
-          <link
-            rel="preload"
-            href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css"
-            as="style"
-            // onLoad="this.onload=null;this.rel='stylesheet'"
-          />
-          <link
-            rel="preload"
-            href="https://unpkg.com/aos@2.3.1/dist/aos.css"
-            as="style"
-            // onLoad="this.onload=null;this.rel='stylesheet'"
-          /> */}
-
-          {/* <noscript>
-            <link
-              href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-              rel="stylesheet"
-            />
-          </noscript> */}
-           {/* curr */}
-          {/* <noscript>
+          <noscript>
             <link
               rel="stylesheet"
               href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
@@ -158,10 +137,7 @@ function MyApp({ Component, pageProps }) {
               href="https://unpkg.com/aos@2.3.1/dist/aos.css"
               rel="stylesheet"
             />
-          </noscript> */}
-          {/* dd */}
-          {/* <link rel="apple-touch-icon" href="/logo192.png" /> */}
-          {/* <link rel="manifest" href="/manifest.json" /> */}
+          </noscript>
         </Head>
         <ToastContainer />
         <Component {...pageProps} />
