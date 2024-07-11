36f4a42692499a47600cf14afbb9e91cb71cc2c1
sib-vishal <vishal.maurya@sibinfo.tech>
2024-07-11 11:56:16 +0530

    commit faqs design added

--- pages/faq.js
+++ pages/faq.js
@@ -1,12 +1,18 @@
-import React, { use, useState } from "react";
-import Accordion from "react-bootstrap/Accordion";
+import React, { useState } from "react";
+
+import { CiCircleMinus } from "react-icons/ci";
+import { CiCirclePlus } from "react-icons/ci";
 import { TbCirclePlus, TbCircleMinus } from "react-icons/tb";
 
-const Faqs = () => {
+const faq = () => {
+  const [open, setOpen] = useState(true);
+  const [open2, setOpen2] = useState(true);
+
+ 
   const faqsData = [
     {
       id: "faq1",
-      question: "What is SEO?",
+      question: "1. What is SEO?",
       answer: ` <div className="accordion-body">
       <p>
         SEO or ‘search engine optimization is the process of
@@ -20,7 +26,7 @@ const Faqs = () => {
     },
     {
       id: "faq2",
-      question: "What are your SEO plans?",
+      question: " 2. What are your SEO plans?",
       answer: `   <div className="accordion-body">
       <p className="customText">
         SEO is a results-oriented process that requires goal
@@ -48,7 +54,7 @@ const Faqs = () => {
     },
     {
       id: "faq3",
-      question: "How long does it take to see the result?",
+      question: "3. How long does it take to see the result?",
       answer: `<p>
       SEO grows over time and the entire SEO process takes
       about 3 to 6 months to see the expected results, with
@@ -61,7 +67,7 @@ const Faqs = () => {
     },
     {
       id: "faq4",
-      question: "What is the cost of SEO services?",
+      question: "4. What is the cost of SEO services?",
       answer: `  <p>
       Our SEO services' cost depends on the client's
       requirements. We provide custom SEO packages
@@ -73,7 +79,7 @@ const Faqs = () => {
     },
     {
       id: "faq5",
-      question: "Why do you need SEO service for your website?",
+      question: "5. Why do you need SEO service for your website?",
       answer: ` <p>
       SEO services offer visibility to your websites and
       improve SEO ranking gradually over search engines like
@@ -84,7 +90,7 @@ const Faqs = () => {
     },
     {
       id: "faq6",
-      question: "Ask for an initial SEO audit of your website?",
+      question: "6. Ask for an initial SEO audit of your website?",
       answer: ` <p>
       We offer free SEO audit services as a part of the
       entire SEO package. We have dedicated professionals to
@@ -97,7 +103,7 @@ const Faqs = () => {
     },
     {
       id: "faq7",
-      question: "How much does SEO Services cost per month?",
+      question: " 7. How much does SEO Services cost per month?",
       answer: ` <p>
       Our SEO services' cost depends on the client's
       requirements. We provide custom SEO packages
@@ -110,7 +116,7 @@ const Faqs = () => {
     },
     {
       id: "faq8",
-      question: `Is SEO worth it in ${new Date().getFullYear()}?`,
+      question: `8. Is SEO worth it in ${new Date().getFullYear()}?`,
       answer: `<p>
       With years crossing by, the emphasis on SEO is never
       degrading instead it is turning more prominent than
@@ -126,7 +132,7 @@ const Faqs = () => {
     },
     {
       id: "faq9",
-      question: `Why SEO services are so expensive?`,
+      question: `9. Why SEO services are so expensive?`,
       answer: `  <div className="accordion-body">
       <p className="customText">
         <strong>
@@ -148,7 +154,7 @@ const Faqs = () => {
     },
     {
       id: "faq10",
-      question: `How do you rank first on Google?`,
+      question: `10. How do you rank first on Google?`,
       answer: `  <p>
       There is no tried and tested method for ranking first
       on Google. However, with the right keywords and SEO
@@ -161,7 +167,7 @@ const Faqs = () => {
     },
     {
       id: "faq11",
-      question: `How does Google rank SEO?`,
+      question: `11. How does Google rank SEO?`,
       answer: `  <p>
       Authority and relevancy are the two primary things
       Google looks for when crawling a website. Google will
@@ -173,7 +179,7 @@ const Faqs = () => {
     },
     {
       id: "faq12",
-      question: `How do I choose a company for SEO?`,
+      question: `12. How do I choose a company for SEO?`,
       answer: `  <div className="accordion-body">
       <p className="customText">
         For choosing a suitable SEO Company for your brand
@@ -199,7 +205,7 @@ const Faqs = () => {
     },
     {
       id: "faq13",
-      question: `What are SEO tools?`,
+      question: `13. What are SEO tools?`,
       answer: ` <p>
       SEO tools determine the potential of your webpage for
       high rankings in search engine results. They provide
@@ -209,7 +215,7 @@ const Faqs = () => {
     },
     {
       id: "faq14",
-      question: `Why Hire SEO Company in India?`,
+      question: `14. Why Hire SEO Company in India?`,
       answer: ` <p>
       Hiring an SEO company in India is a strategic choice
       fueled by a vast pool of skilled professionals focused
@@ -231,57 +237,15 @@ const Faqs = () => {
 
   const faqsDataLeft = faqsData.slice(0, Math.ceil(faqsData.length / 2));
   const faqsDataRight = faqsData.slice(Math.ceil(faqsData.length / 2));
-  const [open, setOpen] = useState(null);
-  const [open2, setOpen2] = useState(null);
 
   return (
-    // <section className="faqsBg">
-    //   <div className="containerFull">
-    //     <h3 className="sub_heading fontWeight500 text-white fontHeading">
-    //       Do You have Questions about Working with SEO Agency in India?{" "}
-    //       <br className="d-none d-lg-block" /> We’ve got your answers.
-    //     </h3>
-    //     <div className="row mt-4">
-    //       <div className="col-lg-6">
-    //         <Accordion defaultActiveKey="0">
-    //           {faqsDataLeft.map((items, i) => {
-    //             return (
-    //               <Accordion.Item className="rounded" key={i} eventKey={i}>
-    //                 <Accordion.Header>{items.question}</Accordion.Header>
-    //                 <Accordion.Body
-    //                   className="text-white"
-    //                   dangerouslySetInnerHTML={{ __html: items.answer }}
-    //                 ></Accordion.Body>
-    //               </Accordion.Item>
-    //             );
-    //           })}
-    //         </Accordion>
-    //       </div>
-    //       <div className="col-lg-6">
-    //         <Accordion defaultActiveKey="0">
-    //           {faqsDataRight.map((items, i) => {
-    //             return (
-    //               <Accordion.Item className="rounded" key={i} eventKey={i}>
-    //                 <Accordion.Header>{items.question}</Accordion.Header>
-    //                 <Accordion.Body
-    //                   className="text-white"
-    //                   dangerouslySetInnerHTML={{ __html: items.answer }}
-    //                 ></Accordion.Body>
-    //               </Accordion.Item>
-    //             );
-    //           })}
-    //         </Accordion>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </section>
     <section>
-      <div className="containerFull ">
-        <h4 className="heading fontHeading mb-4"><span className="text_red">Frequently</span> Asked Questions</h4>
+      <div className="containerFull b">
+        <h4 className="heading fontHeading">Frequently Asked Questions</h4>
         {/* faq */}
 
-        <div className=" row  ">
-          <div className=" col-lg-6 pe-lg-4  ">
+        <div className="b row">
+          <div className=" col-lg-6">
             {faqsDataLeft.map((item, i) => {
               return (
                 <div className="">
@@ -289,11 +253,7 @@ const Faqs = () => {
                     <h4
                       className=" fontHeading"
                       onClick={() => {
-                        if (open !== i) {
-                          setOpen(i);
-                        }else{
-                          setOpen(null);
-                        }
+                        setOpen(i);
                       }}
                     >
                       {item.question}{" "}
@@ -309,9 +269,9 @@ const Faqs = () => {
                       }`}
                     >
                       <div className="overflow-hidden">
-                        <div className="mt-3"
-                          dangerouslySetInnerHTML={{ __html: item.answer }}
-                        ></div>
+                      <div  dangerouslySetInnerHTML={{ __html: item.answer }}>
+
+                       </div>
                       </div>
                     </div>
                   </div>
@@ -319,7 +279,7 @@ const Faqs = () => {
               );
             })}
           </div>
-          <div className=" col-lg-6 ps-lg-4">
+          <div className=" col-lg-6">
             {" "}
             {faqsDataRight.map((item, i) => {
               return (
@@ -328,11 +288,7 @@ const Faqs = () => {
                     <h4
                       className=" fontHeading"
                       onClick={() => {
-                        if (open2 !== i) {
-                          setOpen2(i);
-                        }else{
-                          setOpen2(null);
-                        }
+                        setOpen2(i);
                       }}
                     >
                       {item.question}{" "}
@@ -348,9 +304,9 @@ const Faqs = () => {
                       }`}
                     >
                       <div className="overflow-hidden">
-                        <div className="m-3"
-                          dangerouslySetInnerHTML={{ __html: item.answer }}
-                        ></div>
+                       <div  dangerouslySetInnerHTML={{ __html: item.answer }}>
+
+                       </div>
                       </div>
                     </div>
                   </div>
@@ -364,4 +320,4 @@ const Faqs = () => {
   );
 };
 
-export default Faqs;
+export default faq;
