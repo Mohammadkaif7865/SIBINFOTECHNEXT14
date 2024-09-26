efc982f954d31bf0a3e83d7d32d9c489c0352388
mohammad kaif <mohammadkaig051197@gmail.com>
2024-09-26 12:54:51 +0530

    some changes

--- middleware.js
+++ middleware.js
@@ -1,11 +1,19 @@
-// import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
-// export function middleware(req) {
-//     const { nextUrl } = req;
-//     if (nextUrl.hostname === 'sibinfotech.com') {
-//         const url = req.nextUrl.clone();
-//         url.hostname = 'www.sibinfotech.com';
-//         return NextResponse.redirect(url);
-//     }
-//     return NextResponse.next();
-// }
+export function middleware(req) {
+  const { nextUrl } = req;
+
+  // If already on www.sibinfotech.com, no need to redirect
+  if (nextUrl.hostname === "www.sibinfotech.com") {
+    return NextResponse.next();
+  }
+
+  // Redirect only non-www sibinfotech.com to www.sibinfotech.com
+  if (nextUrl.hostname === "sibinfotech.com") {
+    const url = req.nextUrl.clone();
+    url.hostname = "www.sibinfotech.com";
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
+}
