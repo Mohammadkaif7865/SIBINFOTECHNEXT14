f3bd10e41e2755a63558a3684c9ecf318876a370
mohammad kaif <mohammadkaig051197@gmail.com>
2024-09-26 12:51:54 +0530

    some changes

--- middleware.js
+++ middleware.js
@@ -1,11 +1,11 @@
-import { NextResponse } from 'next/server';
+// import { NextResponse } from 'next/server';
 
-export function middleware(req) {
-    const { nextUrl } = req;
-    if (nextUrl.hostname === 'sibinfotech.com') {
-        const url = req.nextUrl.clone();
-        url.hostname = 'www.sibinfotech.com';
-        return NextResponse.redirect(url);
-    }
-    return NextResponse.next();
-}
+// export function middleware(req) {
+//     const { nextUrl } = req;
+//     if (nextUrl.hostname === 'sibinfotech.com') {
+//         const url = req.nextUrl.clone();
+//         url.hostname = 'www.sibinfotech.com';
+//         return NextResponse.redirect(url);
+//     }
+//     return NextResponse.next();
+// }
