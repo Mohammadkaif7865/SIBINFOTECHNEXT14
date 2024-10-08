ae4723a618badc41b85be61de7aa2c322c30d071
sib-vishal <vishal.maurya@sibinfo.tech>
2024-10-08 17:41:04 +0530

    commit

--- comps/BreadCrumb.js
+++ comps/BreadCrumb.js
@@ -1,14 +1,14 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { IoMdHome } from "react-icons/io";
-import { FaChevronRight } from "react-icons/fa6";
+import { FaAnglesRight, FaChevronRight } from "react-icons/fa6";
 
 const Breadcrumb = () => {
   const router = useRouter();
   const pathSegments = router.asPath.split("/").filter((segment) => segment);
 
   return (
-    <nav className="bgGrey" aria-label="breadcrumb ">
+    <nav className="bgGrey d-flex justify-content-center" aria-label="breadcrumb ">
       <ol className="breadcrumb containerFull ">
         {pathSegments.map((segment, index) => {
           const href = `/${pathSegments.slice(0, index + 1).join("/")}`;
@@ -21,8 +21,9 @@ const Breadcrumb = () => {
               key={index}
               className="breadcrumb-item py-2 d-flex text_truncate align-items-center fontHeading"
             >
-              <IoMdHome className="fs-4" />
-              <FaChevronRight className="ms-1"  style={{
+              {/* <IoMdHome className="fs-4" /> */}
+              Home
+              <FaAnglesRight  className="ms-1"  style={{
                 fontSize:"12px",
               }}/>
               <Link href={href} className="ms-1 text_red fontWeight600 text_truncate">
