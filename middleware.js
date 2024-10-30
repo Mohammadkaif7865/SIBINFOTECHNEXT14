f5b0cc452f2aa1d9aafd94ebe3992591ca5996e9
mohammad kaif <mohammadkaig051197@gmail.com>
2024-10-30 13:30:35 +0530

    some changes

--- middleware.js
+++ middleware.js
@@ -2,21 +2,5 @@
 import { NextResponse } from "next/server";
 
 export function middleware(req) {
-  const { nextUrl } = req;
-
-  // Check if the request is using HTTP and redirect to HTTPS
-  if (nextUrl.protocol === "http:") {
-    const url = req.nextUrl.clone();
-    url.protocol = "https:";
-    return NextResponse.redirect(url);
-  }
-
-  // Redirect from sibinfotech.com to www.sibinfotech.com
-  if (nextUrl.hostname === "sibinfotech.com") {
-    const url = req.nextUrl.clone();
-    url.hostname = "www.sibinfotech.com";
-    return NextResponse.redirect(url);
-  }
-
   return NextResponse.next();
 }
