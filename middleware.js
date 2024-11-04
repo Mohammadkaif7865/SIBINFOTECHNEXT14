28ef6d939dd653a3dfd8daf2c80448423061eeae
mohammad kaif <mohammadkaig051197@gmail.com>
2024-11-04 12:11:56 +0530

    some changes

--- middleware.js
+++ middleware.js
@@ -2,13 +2,5 @@
 import { NextResponse } from "next/server";
 
 export function middleware(req) {
-  const { nextUrl } = req;
-
-  // Check if the request is using HTTP and redirect to HTTPS
-  if (nextUrl.protocol === "http:") {
-    const url = req.nextUrl.clone();
-    url.protocol = "https:";
-    return NextResponse.redirect(url);
-  }
   return NextResponse.next();
 }
