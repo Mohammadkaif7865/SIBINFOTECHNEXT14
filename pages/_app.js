64e5fb6eb8e8269f7dc14f07d879c398ac2e76cd
mohammad kaif <mohammadkaig051197@gmail.com>
2025-04-17 16:39:56 +0530

    some chnages

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,7 @@ import "react-toastify/dist/ReactToastify.css";
 import "../public/assets/css/style.css";
 import "../public/assets/css/responsive.css";
 
-
 import { Inter } from "next/font/google";
-import "@/public/assets/css/style.css"; // Your global CSS
 
 const inter = Inter({
   subsets: ["latin"], // Subset optimizations
@@ -15,76 +13,63 @@ const inter = Inter({
   display: "swap",
 });
 
-
 function MyApp({ Component, pageProps }) {
-  
- 
-
-
   return (
+    <Layout className={inter.className}>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+          crossorigin="anonymous"
+        />
 
-      <Layout className={inter.className}>
-        <Head>
+        <link
+          rel="preload"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+          as="style"
+          onLoad="this.onload=null;this.rel='stylesheet'"
+        />
+        <link
+          rel="preload"
+          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css"
+          as="style"
+          onLoad="this.onload=null;this.rel='stylesheet'"
+        />
+        <link
+          rel="preload"
+          href="https://unpkg.com/aos@2.3.1/dist/aos.css"
+          as="style"
+          onLoad="this.onload=null;this.rel='stylesheet'"
+        />
+        <noscript>
           <link
-            rel="preload"
-            href="/assets/images/banner/banner-video.mp4"
-            as="video/mp4"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
           />
+        </noscript>
+        <noscript>
           <link
             rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-            crossorigin="anonymous"
-          />
-
-          <link
-            rel="preload"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-            as="style"
-            onLoad="this.onload=null;this.rel='stylesheet'"
           />
+        </noscript>
+        <noscript>
           <link
-            rel="preload"
+            rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css"
-            as="style"
-            onLoad="this.onload=null;this.rel='stylesheet'"
           />
+        </noscript>
+        <noscript>
           <link
-            rel="preload"
             href="https://unpkg.com/aos@2.3.1/dist/aos.css"
-            as="style"
-            onLoad="this.onload=null;this.rel='stylesheet'"
+            rel="stylesheet"
           />
-          <noscript>
-            <link
-              href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-              rel="stylesheet"
-            />
-          </noscript>
-          <noscript>
-            <link
-              rel="stylesheet"
-              href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-            />
-          </noscript>
-          <noscript>
-            <link
-              rel="stylesheet"
-              href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css"
-            />
-          </noscript>
-          <noscript>
-            <link
-              href="https://unpkg.com/aos@2.3.1/dist/aos.css"
-              rel="stylesheet"
-            />
-          </noscript>
-        </Head>
-        <ToastContainer />
-        <Component {...pageProps} />
-        
-      </Layout>
-   
+        </noscript>
+      </Head>
+      <ToastContainer />
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
