0a9f2865959be8121b85d1597739f97501f452ec
sib-vishal <vishal.maurya@sibinfo.tech>
2025-04-28 12:20:45 +0530

    redunded section removed

--- comps/SmoPage/Smowhysib.js
+++ comps/SmoPage/Smowhysib.js
@@ -1,6 +1,7 @@
 import React from "react";
+import WhySIB from "../Home/WhySIB";
 
-const WhySIB = () => {
+const SMOWhySIB = () => {
   const keyAdvantages = [
     {
       title: "Increased Brand Visibility ",
@@ -61,65 +62,16 @@ const WhySIB = () => {
   };
 
   return (
-    <section className="bgGrey">
-      <div className="containerFull">
-        <div className="row mt-5">
-          {/* LEFT BOX */}
-          <div className="col-lg-4 mb-3 position-relative">
-            <div className="why_left_item">
-              <h4 className="fontHeading2 heading fontWeight300 text_red">
-                {leftBoxContent.headingStart}{" "}
-                <span className="fontWeight600">{leftBoxContent.headingHighlight}</span>
-              </h4>
-              <h5 className="mt-3 sub_heading fontHeading2 fontWeight600">
-                {leftBoxContent.subHeading}
-                <span className="text_red">.</span>
-              </h5>
-              <div className="why_left_card mt-4 text-center">
-                <h4 className="fontHeading2 text-white fontWeight600">
-                  {leftBoxContent.cardTitle}
-                </h4>
-                <p className="mt-3 text-white">{leftBoxContent.cardDescription}</p>
-                <button className="p-2 mt-4 w-100 btnPrimary text-white">
-                  {leftBoxContent.buttonText}
-                </button>
-              </div>
-            </div>
-          </div>
 
-          {/* RIGHT BOX */}
-          <div className="col-lg-8">
-            <div className="rightWhyBlock">
-              <div className="headerRightWhy py-lg-3 bgGrey">
-                <h3 className="fontWeight600 text_red font ps-4">
-                  {rightBoxContent.heading}
-                </h3>
-                <p className="mt-3 ps-4">{rightBoxContent.description}</p>
-              </div>
-              <div className="row mt-4">
-                {keyAdvantages.map((value, index) => (
-                  <div className="col-lg-6 mb-3 position-relative" key={index}>
-                    <div className="item_why_sib hover h-100">
-                      <div className="item_why_title align-items-center d-flex justify-content-between">
-                        <h4 className="small_heading fontHeading2 fontWeight600">
-                          {value.title}
-                        </h4>
-                        <h5 className="fontHeading2 fontWeight600">
-                          {(index + 1).toString().padStart(2, "0")}
-                        </h5>
-                      </div>
-                      <p className="mt-4">{value.description}</p>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-          
-        </div>
-      </div>
-    </section>
+    
+    <WhySIB  
+            keyAdvantages={keyAdvantages}
+            leftBoxContent={leftBoxContent}
+            rightBoxContent={rightBoxContent}
+          />
+  
+
   );
 };
 
-export default WhySIB;
+export default SMOWhySIB;
