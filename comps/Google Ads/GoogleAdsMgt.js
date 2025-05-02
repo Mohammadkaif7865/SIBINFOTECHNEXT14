1d2c8d842939cac5b3952096ed72ccba73f2c41b
Neha-vis <nehacollege12@gmail.com>
2025-05-02 18:25:34 +0530

    googleads-uichanges

--- comps/Google Ads/GoogleAdsMgt.js
+++ comps/Google Ads/GoogleAdsMgt.js
@@ -120,60 +120,7 @@ buttonText: "Get a Free Analysis",
   const subheading = "We offer Best Google Ads Management  Services in India";
   // const description =
   //   "Get the most out of your budget with our expert-managed PPC campaigns across multiple platforms.";
-  const tabs = [
-    {
-      title: "Google Ads Management",
-      content: `
-        <p><strong>Search Ads</strong></p>
-        <p>The Search Network campaign is the only platform that allows your business to display text ads on the SERPs (Search Engine Result Pages). The campaign is supplemented with extensive features that include app installs, mobile app engagement, dynamic search ads, call-only or callout, and snippets. SN is a boundless place for a new Google Ads user to advertise for wide-ranging businesses along with the local business. Search Network campaigns are beneficial when:</p>
-        <ul>
-          <li>High-intent keyword targeting for maximum visibility.</li>
-          <li>Ad copywriting and split testing for improved CTR.</li>
-          <li>Immediate traffic and leads.</li>
-        </ul>
   
-        <p><strong>Display Ads</strong></p>
-        <p>The image ads are displayed on the Google Display Network (GDN), which consists of millions of websites to show your ads. The GDN campaigns demand to setup marketing objectives that can be based on impressions, visits, engagement, and actions. The objectives you select are the basis for building your ad campaign and its success. You can use the GDN campaign:</p>
-        <ul>
-          <li>Visually appealing banner ads to reach a broader audience.</li>
-          <li>Strategic placements on high-traffic websites.</li>
-          <li>Broader audience coverage to appear on various websites and apps.</li>
-        </ul>
-  
-        <p><strong>Shopping Ads (Google Shopping)</strong></p>
-        <p>The first step for a Google shopping campaign is to set up Google Merchant Centre. This ad campaign is useful to showcase your products from your e-commerce store in Google Shopping. The success of the campaign depends on how you set up your e-commerce store or website, if the setup is correct and Google receives correct feeds, it does all the targeting and ad creation jobs on its own. The Google Shopping Ads campaign could only be used if you have an e-commerce store. It is used for:</p>
-        <ul>
-          <li>Optimized product listings for eCommerce businesses.</li>
-          <li>Increased visibility on Google Shopping results.</li>
-          <li>Reach ready to buy shoppers for higher conversions.</li>
-        </ul>
-  
-        <p><strong>YouTube Advertising</strong></p>
-        <p>There are several ad formats available for video ad campaigns on Google Ads. The options range from letting the viewer skip your ad after five seconds to a six seconds buffer ad between videos. The campaign has several features that allow you to extend your ads with shopping campaigns along with app installs. It is effective for:</p>
-        <ul>
-          <li>Engaging video ads to target the right audience.</li>
-          <li>Skippable and non-skippable ads to boost brand reach.</li>
-          <li>Re-engage audience who have previously interacted with the brand.</li>
-        </ul>
-  
-        <p><strong>Remarketing & Retargeting</strong></p>
-        <p>The Google remarketing ads are considered to be a powerful tool for businesses as it is useful in targeting the people who have already visited your store or used your application. The campaign demands careful set-up as it leads to the generation of your remarketing tags and lists. The campaign is effective for:</p>
-        <ul>
-          <li>Re-engage visitors and convert lost leads into customers.</li>
-          <li>Personalized ad strategies for higher retention.</li>
-          <li>Keeps the brands visible across websites and social media.</li>
-        </ul>
-  
-        <p><strong>Performance Max Campaigns</strong></p>
-        <p>Performance Max campaign ensures that your ads reach the right audience at the right time. It is a goal-oriented campaign, ideal for driving sales and increasing brand visibility. It delivers higher ROI and better results effortlessly. It is used for:</p>
-        <ul>
-          <li>AI-driven campaigns for multi-channel ad placements.</li>
-          <li>Optimized bidding strategies to enhance conversions.</li>
-          <li>Asset-based approach that creates high-performing ads.</li>
-        </ul>
-      `,
-    },
-  ];
 
   const about3Data = {
     sectionId: "about",
@@ -207,55 +154,7 @@ buttonText: "Get a Free Analysis",
     callIcon: "/assets/images/icons/call-icon.svg",
   };
 
-  const agencyWorkflow = [
-    {
-      title: "Initial Assessment & Audit ",
-      description: `
-        <ul class="ourapproach_ul">
-          <li>Platform Selection </li>
-          <li>Identify Opportunities </li>
-          <li>Social Audit + Recommendations </li>
-          <li>Competitor analysis  </li>
-          
-        </ul>
-      `,
-    },
-    {
-      title: "Strategy Development ",
-      description: `
-        <ul class="ourapproach_ul">
-          <li>Social Profile Account Setup </li>
-          <li>Content Calendar Creation </li>
-          <li>Hashtags Management </li>
-          <li>Target Audience Analysis </li>
-         
-          
-        </ul>
-      `,
-    },
-    {
-      title: "Engagement & Growth ",
-      description: `
-        <ul class="ourapproach_ul">
-          <li>Design Appeal Visuals  </li>
-          <li>Social Post Scheduling  </li>
-          <li>Boosting / Group sharing   </li>
-          <li>Follower Engagement </li>
-        </ul>
-      `,
-    },
-    {
-      title: "Monitoring & Reporting ",
-      description: `
-        <ul class="ourapproach_ul">
-          <li>Account Monitoring </li>
-          <li>Likes, Comments / Engagement Monitoring </li>
-          <li>Enhances Consistency </li>
-          <li>Feedback and Adaptation </li>
-        </ul>
-      `,
-    },
-  ];
+  
   return (
     <>
       <HeroGoogleAds />
@@ -274,7 +173,7 @@ buttonText: "Get a Free Analysis",
         data={smoServices}
       />
       
-      <Partnership
+      {/* <Partnership
         batchtitle="Our Approach"
         agencyWorkflow={agencyWorkflow}
         title="Complete SMO Process"
@@ -286,7 +185,7 @@ By optimizing profiles, using the right hashtags, participating in discussions, 
 Ultimately, SMO helps in reaching a broader audience organically and builds a loyal community around the brand.
 
 "
-      />
+      /> */}
      
 
       <AdsCompany />
