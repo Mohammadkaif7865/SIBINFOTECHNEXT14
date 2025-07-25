82b173c8e3d0f0527b34d53ff9abfd305d6129a0
sibinfotechgit <sibinfotech101@gmail.com>
2025-07-25 17:07:59 +0530

    author

--- pages/_app.js
+++ pages/_app.js
@@ -3,24 +3,22 @@ import Script from 'next/script';
 import Layout from "../comps/Layout";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "../public/assets/css/style.css";
-import "../public/assets/css/responsive.css";
+import "../styles/style.css";
+import "../styles/responsive.css";
 
-import "../public/assets/css/blog.css";
-import "../public/assets/css/chatbot.css";
+import "../styles/blog.css";
+import "../styles/chatbot.css";
 
-import { Inter } from "next/font/google";
-
-const inter = Inter({
-  subsets: ["latin"], // Subset optimizations
-  weight: ["400", "700"], // Define font weights
-  display: "swap",
-});
 // test
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout className={inter.className}>
+    <Layout>
       <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Lora:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+
         <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
