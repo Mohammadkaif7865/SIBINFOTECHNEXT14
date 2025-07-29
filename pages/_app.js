ed25a42f4c53da1f18325c809af05c69307666a9
sibinfotechgit <sibinfotech101@gmail.com>
2025-07-29 17:58:41 +0530

    author

--- pages/_app.js
+++ pages/_app.js
@@ -19,12 +19,20 @@ function MyApp({ Component, pageProps }) {
           rel="stylesheet"
         />
 
-        <link
+        <link 
+          rel="preload" 
+          as="style" 
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" 
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" 
+          crossorigin="anonymous" 
+          onload="this.onload=null;this.rel='stylesheet'" />
+
+        {/* <link
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
           integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
           crossorigin="anonymous"
-        />
+        /> */}
 
         <link
           rel="preload"
@@ -44,12 +52,22 @@ function MyApp({ Component, pageProps }) {
           as="style"
           onLoad="this.onload=null;this.rel='stylesheet'"
         />
-        <noscript>
+
+        {/* <noscript>
           <link
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
             rel="stylesheet"
           />
+        </noscript> */}
+
+        <noscript>
+          <link 
+            rel="stylesheet" 
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" 
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" 
+            crossorigin="anonymous" />
         </noscript>
+
         <noscript>
           <link
             rel="stylesheet"
