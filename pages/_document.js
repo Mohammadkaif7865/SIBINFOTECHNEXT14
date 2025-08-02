37592a9eeb5174a900dc51c83984717ef4d447ae
sibinfotechgit <sibinfotech101@gmail.com>
2025-08-02 11:06:01 +0530

    page speed

--- pages/_document.js
+++ pages/_document.js
@@ -7,17 +7,28 @@ class MyDocument extends Document {
     return (
       <Html lang="en" >
         <Head>
+          {/* Preconnects */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link rel="preconnect" href="https://cdn.jsdelivr.net" crossOrigin="anonymous" />
           <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
           <link rel="preconnect" href="https://unpkg.com" crossOrigin="anonymous" />
 
+          {/* Google Fonts */}
           <link
-            rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Lora:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
           />
 
+          {/* Bootstrap 5.3.2 CSS */}
+          <link
+            rel="preload"
+            as="style"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            onload="this.onload=null;this.rel='stylesheet'"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossOrigin="anonymous"
+          />
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
@@ -25,21 +36,51 @@ class MyDocument extends Document {
             crossOrigin="anonymous"
           />
 
+          {/* Font Awesome */}
+          <link
+            rel="preload"
+            as="style"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+            onload="this.onload=null;this.rel='stylesheet'"
+          />
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
           />
 
+          {/* Bootstrap Icons */}
+          <link
+            rel="preload"
+            as="style"
+            href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css"
+            onload="this.onload=null;this.rel='stylesheet'"
+          />
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css"
           />
 
+          {/* AOS Animation CSS */}
+          <link
+            rel="preload"
+            as="style"
+            href="https://unpkg.com/aos@2.3.1/dist/aos.css"
+            onload="this.onload=null;this.rel='stylesheet'"
+          />
           <link
             rel="stylesheet"
             href="https://unpkg.com/aos@2.3.1/dist/aos.css"
           />
 
+          {/* Bootstrap 5.0.2 CSS (if still needed for legacy layout) */}
+          <link
+            rel="preload"
+            as="style"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+            onload="this.onload=null;this.rel='stylesheet'"
+            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+            crossOrigin="anonymous"
+          />
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
