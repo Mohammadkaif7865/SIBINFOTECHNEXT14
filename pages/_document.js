b8641742a5c476d1ec3812a452dc67038a9155fa
sibinfotechgit <sibinfotech101@gmail.com>
2025-08-02 10:58:23 +0530

    page speed

--- pages/_document.js
+++ pages/_document.js
@@ -7,55 +7,45 @@ class MyDocument extends Document {
     return (
       <Html lang="en" >
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link rel="preconnect" href="https://cdn.jsdelivr.net" crossOrigin="anonymous" />
+          <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
+          <link rel="preconnect" href="https://unpkg.com" crossOrigin="anonymous" />
 
-
-        {/* Google Fonts: Poppins & Lora */}
           <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Lora:wght@400;500;600;700&display=swap"
             rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Lora:wght@400;500;600;700&display=swap"
           />
 
-          {/* Bootstrap 5.3.2 CSS (Preloaded and deferred) */}
           <link
-            rel="preload"
-            as="style"
+            rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-            onLoad="this.onload=null;this.rel='stylesheet'"
             integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
             crossOrigin="anonymous"
           />
 
-          {/* Font Awesome */}
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
           />
 
-          {/* Bootstrap Icons */}
           <link
-            rel="preload"
-            as="style"
+            rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css"
-            onLoad="this.onload=null;this.rel='stylesheet'"
           />
 
-          {/* AOS Animation CSS */}
           <link
-            rel="preload"
-            as="style"
+            rel="stylesheet"
             href="https://unpkg.com/aos@2.3.1/dist/aos.css"
-            onLoad="this.onload=null;this.rel='stylesheet'"
           />
 
-          {/** Optional fallback if you need legacy layout support with Bootstrap 5.0.2 */}
-          {
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
             integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
             crossOrigin="anonymous"
           />
-          }
 
           {/* Google Tag Manager */}
           <Script
