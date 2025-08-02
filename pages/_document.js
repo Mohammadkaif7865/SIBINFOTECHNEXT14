a6f0d00ac3450738de72df514ef2ef0372c341ad
sibinfotechgit <sibinfotech101@gmail.com>
2025-08-02 12:22:19 +0530

    page speed

--- pages/_document.js
+++ pages/_document.js
@@ -21,13 +21,11 @@ class MyDocument extends Document {
             rel="preload"
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
             as="style"
-            onLoad="this.onload=null;this.rel='stylesheet'"
+            onload="this.onload=null;this.rel='stylesheet'"
           />
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-            crossOrigin="anonymous"
           />
 
           {/* Font Awesome */}
