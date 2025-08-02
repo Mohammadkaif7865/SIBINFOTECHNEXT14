849d7f6051574b77028853cd30f40a2d925e94e2
sibinfotechgit <sibinfotech101@gmail.com>
2025-08-02 11:24:57 +0530

    page speed

--- pages/_document.js
+++ pages/_document.js
@@ -1,34 +1,22 @@
-// pages/_document.js
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import Script from 'next/script';
+import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en" >
+      <Html lang="en">
         <Head>
-          {/* Preconnects */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link rel="preconnect" href="https://cdn.jsdelivr.net" crossOrigin="anonymous" />
           <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
           <link rel="preconnect" href="https://unpkg.com" crossOrigin="anonymous" />
 
-          {/* Google Fonts */}
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Lora:wght@400;500;600;700&display=swap"
             rel="stylesheet"
           />
 
-          {/* Bootstrap 5.3.2 CSS */}
-          <link
-            rel="preload"
-            as="style"
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-            onload="this.onload=null;this.rel='stylesheet'"
-            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-            crossOrigin="anonymous"
-          />
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
@@ -36,62 +24,22 @@ class MyDocument extends Document {
             crossOrigin="anonymous"
           />
 
-          {/* Font Awesome */}
-          <link
-            rel="preload"
-            as="style"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-            onload="this.onload=null;this.rel='stylesheet'"
-          />
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
           />
-
-          {/* Bootstrap Icons */}
-          <link
-            rel="preload"
-            as="style"
-            href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css"
-            onload="this.onload=null;this.rel='stylesheet'"
-          />
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css"
-          />
-
-          {/* AOS Animation CSS */}
-          <link
-            rel="preload"
-            as="style"
-            href="https://unpkg.com/aos@2.3.1/dist/aos.css"
-            onload="this.onload=null;this.rel='stylesheet'"
           />
           <link
             rel="stylesheet"
             href="https://unpkg.com/aos@2.3.1/dist/aos.css"
           />
 
-          {/* Bootstrap 5.0.2 CSS (if still needed for legacy layout) */}
-          <link
-            rel="preload"
-            as="style"
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-            onload="this.onload=null;this.rel='stylesheet'"
-            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-            crossOrigin="anonymous"
-          />
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-            integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-            crossOrigin="anonymous"
-          />
-
-          {/* Google Tag Manager */}
           <Script
             id="gtm-script"
-            strategy="lazyOnload"
+            strategy="afterInteractive"
             dangerouslySetInnerHTML={{
               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -100,7 +48,6 @@ class MyDocument extends Document {
               })(window,document,'script','dataLayer','GTM-WSSFNZR');`,
             }}
           />
-          {/* End Google Tag Manager */}
 
           <script
             type="application/ld+json"
@@ -114,7 +61,6 @@ class MyDocument extends Document {
               }),
             }}
           />
-
           <script
             type="application/ld+json"
             dangerouslySetInnerHTML={{
@@ -143,55 +89,50 @@ class MyDocument extends Document {
               }),
             }}
           />
-
           <script
-              type="application/ld+json"
-              dangerouslySetInnerHTML={{
-                __html: JSON.stringify({
-                  "@context": "https://schema.org",
-                  "@type": "LocalBusiness",
-                  name: "SIB Infotech",
-                  url: "https://www.sibinfotech.com/",
-                  image: "https://www.sibinfotech.com/assets/og/sib-infotech.png",
-                  priceRange: "₹5,000 - ₹500,000",
-                  telephone: "+91 92222 60000",
-                  email: "contact@sibinfotech.com",
-                  address: {
-                    "@type": "PostalAddress",
-                    streetAddress:
-                      "107, Orbit Premises, Mindspace Near Inorbit Mall, Malad West",
-                    addressLocality: "Mumbai, Maharashtra",
-                    addressCountry: "India",
-                    postalCode: "400064",
-                  },
-                  aggregateRating: {
-                    "@type": "AggregateRating",
-                    ratingValue: "4.4",
-                    bestRating: "5",
-                    reviewCount: "200",
-                  },
-                  openingHours: ["Mon-Sat 09:30 AM - 06:30 PM"],
-                }),
-              }}
-            />
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{
+              __html: JSON.stringify({
+                "@context": "https://schema.org",
+                "@type": "LocalBusiness",
+                name: "SIB Infotech",
+                url: "https://www.sibinfotech.com/",
+                image: "https://www.sibinfotech.com/assets/og/sib-infotech.png",
+                priceRange: "₹5,000 - ₹500,000",
+                telephone: "+91 92222 60000",
+                email: "contact@sibinfotech.com",
+                address: {
+                  "@type": "PostalAddress",
+                  streetAddress:
+                    "107, Orbit Premises, Mindspace Near Inorbit Mall, Malad West",
+                  addressLocality: "Mumbai, Maharashtra",
+                  addressCountry: "India",
+                  postalCode: "400064",
+                },
+                aggregateRating: {
+                  "@type": "AggregateRating",
+                  ratingValue: "4.4",
+                  bestRating: "5",
+                  reviewCount: "200",
+                },
+                openingHours: ["Mon-Sat 09:30 AM - 06:30 PM"],
+              }),
+            }}
+          />
 
           <Script
             id="clarity-script"
             strategy="lazyOnload"
             dangerouslySetInnerHTML={{
-              __html: `
-                (function(c,l,a,r,i,t,y){
-                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                })(window, document, "clarity", "script", "semoba83rl");
-              `,
+              __html: `(function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "semoba83rl");`,
             }}
           />
-
         </Head>
         <body>
-          {/* Google Tag Manager (noscript) */}
           <noscript>
             <iframe
               src="https://www.googletagmanager.com/ns.html?id=GTM-WSSFNZR"
@@ -200,9 +141,6 @@ class MyDocument extends Document {
               style={{ display: "none", visibility: "hidden" }}
             ></iframe>
           </noscript>
-
-
-          {/* End Google Tag Manager (noscript) */}
           <Main />
           <NextScript />
         </body>
