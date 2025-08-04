232e6e6c070f1dedf4c46fca9b3c5d3d342b823a
sibinfotechgit <sibinfotech101@gmail.com>
2025-08-04 18:22:53 +0530

    page speed

--- pages/_document.js
+++ pages/_document.js
@@ -6,57 +6,57 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link rel="preconnect" href="https://cdn.jsdelivr.net" crossOrigin="anonymous" />
           <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
-          <link rel="preconnect" href="https://unpkg.com" crossOrigin="anonymous" />
+          <link rel="preconnect" href="https://unpkg.com" crossOrigin="anonymous" /> */}
 
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Lora:wght@400;500;600;700&display=swap"
             rel="stylesheet"
           />
 
-          <link
+          {/* <link
             rel="preload"
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
             as="style"
             onload="this.onload=null;this.rel='stylesheet'"
-          />
+          /> */}
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
           />
 
           {/* Font Awesome */}
-          <link
+          {/* <link
             rel="preload"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
             as="style"
             onLoad="this.onload=null;this.rel='stylesheet'"
-          />
+          /> */}
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
           />
 
-          <link
+          {/* <link
             rel="preload"
             href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css"
             as="style"
             onLoad="this.onload=null;this.rel='stylesheet'"
-          />
+          /> */}
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css"
           />
 
-          <link
+          {/* <link
             rel="preload"
             href="https://unpkg.com/aos@2.3.1/dist/aos.css"
             as="style"
             onLoad="this.onload=null;this.rel='stylesheet'"
-          />
+          /> */}
           <link
             rel="stylesheet"
             href="https://unpkg.com/aos@2.3.1/dist/aos.css"
